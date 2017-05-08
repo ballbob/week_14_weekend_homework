5af8095a30c66a2f44921931c4a70e77627f05d3
Robert Ball <robert.ball.1989@gmail.com>
2017-05-08 09:09:52 +0100

    Current point: setting up secondary menu.

--- client/src/components/Face.jsx
+++ client/src/components/Face.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 class Face extends React.Component{
   constructor(props){
     super(props)
+    this.inPlay = true
+  }
+
+  notInPlay(){
+    this.inPlay = false
   }
 
   render(){
--- client/src/containers/QuestionContainer.jsx
+++ client/src/containers/QuestionContainer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 class QuestionContainer extends React.Component{
+  
+  handleChange(){
+   let attribute = document.querySelector('#attribute')
+   let option = document.querySelector('#option')
+
+   if(this.state.)
+  }
+
   render(){
+
     return(
-      <p>Question Container is here.</p>
+      <div id="question-container" onClick={this.handleChange}>
+        <select id="attribute">
+          <option>Hair</option>
+          <option>Eyes</option>
+          <option>Glasses</option>
+        </select>
+
+        <select id="option">
+        </select>
+      </div>
     )
   }
 }
